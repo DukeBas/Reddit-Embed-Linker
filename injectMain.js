ff721c486318d58c6157b1b5a1fe4ab04cde59b2
DukeBas <35384117+DukeBas@users.noreply.github.com>
2021-07-13 21:08:30 +0200

    File structure, link button on main page

---
+++ injectMain.js
@@ -0,0 +1,43 @@
+console.log("Running main!");
+
+// use an observer to detect when the body style changes
+// when it does call function to add embed link button if 
+// a post is in focus
+const target = document.getElementsByTagName('body')[0];
+const observer = new MutationObserver(function (mutations) {
+    mutations.forEach(function (mutationRecord) {
+        if (target.style.overflow == "hidden") {
+            // a post is in focus, add link button
+            postInFocus();
+        }
+    });
+});
+observer.observe(target, { attributes: true, attributeFilter: ['style'] });
+
+console.log(target)
+
+function postInFocus() {
+    console.log("Post in focus!");
+    // const candidates = getElementsByTextInclusion('% Upvoted');
+    // console.log(candidates);
+
+    // setTimeout(() => {
+    //     const candidates = getElementsByTextInclusion('% Upvoted');
+    //     console.log(candidates);
+    // }, 200);
+
+    // check every 200ms for an upvoted % span element
+    let candidates = [];
+    function spanCheck() {
+        candidates = getElementsByTextInclusion('% Upvoted');
+        if (candidates.length > 0) {
+            // span is created, continue to next step
+            try {
+                clearInterval(check);   // clear timer if it exists
+            } catch { }
+            addLinkButton();
+        }
+    }
+    let check = setInterval(spanCheck, 200);
+}
+
